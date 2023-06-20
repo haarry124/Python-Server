a99af153e7eacc730330e58ded2d772277a8450f
Harshal Dod <132652808+haarry124@users.noreply.github.com>
2023-06-21 01:44:02 +0530

    Change server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello, world!")
+
+def run(server_class=HTTPServer, handler_class=MyServer, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
+
+run()
